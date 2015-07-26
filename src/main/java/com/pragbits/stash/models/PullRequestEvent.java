c199687de4db0aafa453718633baa718f9acd882
Dominic Boutin <dominic.boutin@equisoft.com>
2015-07-26 17:18:08 -0400

    Add PullRequestMergeability to PullRequest entity

--- src/main/java/com/pragbits/stash/models/PullRequestEvent.java
+++ src/main/java/com/pragbits/stash/models/PullRequestEvent.java
@@ -1,16 +1,19 @@
 package com.pragbits.stash.models;
 
 import com.atlassian.stash.nav.NavBuilder;
+import com.atlassian.stash.pull.PullRequestService;
 
 public class PullRequestEvent {
     public String action;
     public StashUser user;
     public PullRequest pullRequest;
 
-    public PullRequestEvent(com.atlassian.stash.event.pull.PullRequestActivityEvent event, NavBuilder navBuilder)
+    public PullRequestEvent(com.atlassian.stash.event.pull.PullRequestActivityEvent event,
+                            NavBuilder navBuilder,
+                            PullRequestService pullRequestService)
     {
         action = event.getAction().name();
         user = new StashUser(event.getUser(), navBuilder);
-        pullRequest = new PullRequest(event.getPullRequest(), navBuilder);
+        pullRequest = new PullRequest(event.getPullRequest(), navBuilder, pullRequestService);
     }
 }
